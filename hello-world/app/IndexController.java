5c80582943882b0420ccf26356f8308c0156797d
gans <vehpsrr@i.ua>
2014-07-14 21:09:58 +0300

    paly wiht lw tag

--- hello-world/app/IndexController.java
+++ hello-world/app/IndexController.java
@@ -3,6 +3,6 @@ package com.gans.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("some minore changes");
+		System.out.println("play with lv tags");
 	}
 }
