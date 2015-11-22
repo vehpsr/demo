29103531cfa4d6871cb2b24f31ccf004e2928989
shusak <shusak@cvent.com>
2015-11-22 18:31:27 +0200

    final merge of two lines (sqashy one)

--- hello-world/app/IndexController.java
+++ hello-world/app/IndexController.java
@@ -7,5 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("play with lv tags");
 		System.out.println("new line of code without bug");
+		/*
+		Line of comment: breaking change.
+		*/
 	}
 }
