2f12ffe59b5d2af69765c10910566ab36cc2e207
SHusak <SHusak@cvent.com>
2016-10-03 17:40:53 +0300

    added new property; tabs to spaces

--- hello-world/app/IndexController.java
+++ hello-world/app/IndexController.java
@@ -1,11 +1,11 @@
 package com.gans.test;
 
 public class Main {
-	/*
-	* added line of comment. check line ending 1
-	*/
-	public static void main(String[] args) {
-		System.out.println("play with lv tags");
-		System.out.println("new line of code without bug");
-	}
+    /*
+     * added line of comment. check line ending 1
+     */
+    public static void main(String[] args) {
+        System.out.println("play with lv tags");
+        System.out.println("new line of code without bug");
+    }
 }
