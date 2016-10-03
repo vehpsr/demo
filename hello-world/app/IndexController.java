743fd7eb250a53c263c6e8ae66b97c6c80044891
SHusak <SHusak@cvent.com>
2016-10-03 17:53:13 +0300

    unstage. partial commit

--- hello-world/app/IndexController.java
+++ hello-world/app/IndexController.java
@@ -5,7 +5,7 @@ public class Main {
      * added line of comment. check line ending 1
      */
     public static void main(String[] args) {
-        System.out.println("i try learn to unstage");
+        System.out.println("i try learn to unstage 2");
         System.out.println("new line of code without bug");
     }
 }
