2780d24b388486849a6a0c87189316544683635e
SHusak <SHusak@cvent.com>
2016-10-03 17:48:57 +0300

    unstage. pt1

--- hello-world/app/IndexController.java
+++ hello-world/app/IndexController.java
@@ -5,7 +5,7 @@ public class Main {
      * added line of comment. check line ending 1
      */
     public static void main(String[] args) {
-        System.out.println("play with lv tags");
+        System.out.println("i try learn to unstage");
         System.out.println("new line of code without bug");
     }
 }
